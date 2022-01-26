b9f52c7197dc3fe7ddd2f998bc28209cc241a550
sunxinyi0501 <sunxinyi@udel.edu>
2022-01-26 15:45:52 -0500

    Bit change , add the starup page

--- js/banner_data.js
+++ js/banner_data.js
@@ -2,7 +2,7 @@ var banner_data=[
 	{
 		id:1,
 		name:'movie1',
-		banner:'./img/movies/mov.jfif',
-		banner_s:'./img/movies/mov.jfif',
+		banner:'./img/movies/banner.jpeg',
+		banner_s:'./img/movies/banner_s.jpeg',
 	}
 ]
--- js/movie_data.js
+++ js/movie_data.js
@@ -3,14 +3,14 @@ var movie_data=[
 		id:'1',
 		bannerInfo:{
 			id:1,
-			banner:'./img/movies/1.jfif',
-			banner_s:'./img/movies/1-1.jfif',
+			banner:'./img/movies/banner.jpeg',
+			banner_s:'./img/movies/banner_s.jpeg',
 		},
 		isVip:true,
 		time:'01:42:25',
 		name:'Jian Feng Dui Jue ',
 		score:7,
-		tags:['2019','Adventure','Action'],
+		tags:['2019','Action'],
 		desc:'introduction'
 	}
 ]
